0fbf3a7c7155801dffba672198a231c58cd6371d
Tommy O'Neil <alieander@gmail.com>
2018-08-30 17:45:09 -0700

    work cleanup

--- fastly/aliases.zsh
+++ fastly/aliases.zsh
@@ -1,7 +1,1 @@
 alias stg='ssh app-stg-sjc3543'
-
-alias resume='vagrant resume 90999d9'
-alias halt='vagrant halt 90999d9'
-alias start='vagrant start 90999d9'
-alias up='vagrant up 90999d9'
-alias pauper='chruby system; pauper'
