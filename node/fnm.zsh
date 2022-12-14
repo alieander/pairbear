47baa0599ca94abf3f336c26cb85a083350e70eb
Tommy ONeil <toneil@easypost.com>
2022-12-14 14:22:14 -0800

    check for fnm first

--- node/fnm.zsh
+++ node/fnm.zsh
@@ -1,1 +1,5 @@
-eval "$(fnm env --use-on-cd --version-file-strategy=recursive)"
+fnm_path=$(which fnm) >/dev/null 2>&1
+if [[ -f $fnm_path ]]
+then
+  eval "$(fnm env --use-on-cd --version-file-strategy=recursive)"
+fi
